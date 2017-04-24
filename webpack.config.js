b2e96d1a4bcce346f7941b139eecfb6363e6741e
secabhi <abhiatstudy@gmail.com>
2017-04-25 00:00:30 +0530

    npm script fix webconfig ie issue fix

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ var DIST_DIR = path.resolve(__dirname,'dist');
 var SRC_DIR = path.resolve(__dirname,'src');
 
 var config = {
-	entry:SRC_DIR+'/app/index.js',
+	entry:['babel-polyfill',SRC_DIR+'/app/index.js'],
 	output:{
 		path:DIST_DIR+"/app",
 		filename:"bundle.js",
