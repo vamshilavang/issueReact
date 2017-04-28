6920ec9a860dea5ce35175ebd845b178350803ae
secabhi <abhiatstudy@gmail.com>
2017-04-28 06:39:52 +0530

    errorMessage new design added

--- src/app/components/eMenu/reqProvider/question.js
+++ src/app/components/eMenu/reqProvider/question.js
@@ -4,16 +4,22 @@ import _ from 'underscore';
 import Radio from '../../common/radioBtn'
 import Select from '../../common/select'
 import TextBox from '../../common/textbox';
+import ErrorMsg from '../../common/errormsg';
 
 const Question = (props) => {
     console.log("choices");
 
     return (
         <div>
+            {props.data.Value == '' || props.data.Value == null?
+                <span>
 
-                <span>  {(props.data.FieldValues != undefined && props.data.FieldValues.length >0)||props.data.ControlType == 'Textbox' ?
-                    <div>{props.data.Caption}</div>:null}
-                    <form>
+                <ErrorMsg Caption={props.data.Caption}/></span>:null
+
+            }
+            <span>  {(props.data.FieldValues != undefined && props.data.FieldValues.length >0)||props.data.ControlType == 'Textbox' ?
+                <div>{props.data.Caption}</div>:null}
+                <form>
                         <div className="radio" style={{ marginTop: '0px' }}>
                             <div className="control-group" style={{ padding: '0px' }}>
                                 {props.data.FieldValues != undefined && props.data.FieldValues.length <= 4 ?
@@ -21,9 +27,7 @@ const Question = (props) => {
                                         return <Radio key={props.clientproductId + "-" + i} caption={props.data.Caption} data={c} categoryName={props.categoryName} clientProductId={props.clientproductId} selected={props.data.Value == c.Code ? true : false} qId={props.qId} events={props.events.eMenuOptionselect} />
                                     })
                                     : <Select data={props.data} categoryName={props.categoryName} caption={props.data.Caption}
-                                                clientProductId={props.clientproductId} qId={props.qId} events={props.events.eMenuOptionselect} />
-
-
+                                              clientProductId={props.clientproductId} qId={props.qId} events={props.events.eMenuOptionselect} />
                                 }
                                 {props.data.ControlType == 'Textbox'?
                                     <TextBox data={props.data} categoryName={props.categoryName} caption={props.data.Caption}
